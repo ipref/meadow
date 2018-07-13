8099f8f0a3479fc70043fc196aa9a8fbc5df116f
waldemar <waldemar@wdmsys.com>
2018-07-13 08:51:12 -0700

    move cli to config crate

---
+++ src/config.rs
@@ -0,0 +1,51 @@
+/* Copyright (c) 2018 Waldemar Augustyn */
+
+
+use clap::{App, Arg, SubCommand};
+use std::process;
+
+pub struct Config {
+    pub debug: bool,
+    pub port: u16,
+}
+
+pub fn get() -> Config {
+
+    // read cli
+
+    let cli = App::new(crate_name!())
+                    .version(crate_version!())
+                    .about("Run IPREF gateway in user space")
+                    .help_message("display this help message")
+                    .version_message("display version")
+                    .arg(Arg::with_name("config")
+                        .short("c")
+                        .long("config")
+                        .value_name("CONFIG")
+                        .default_value("/etc/ipref/ipref.conf")
+                        .help("absolute path to config file")
+                        .takes_value(true))
+                    .args_from_usage("-d, --debug 'enable debug'")
+                    .subcommand(SubCommand::with_name("start")
+                        .about("start IPREF gateway"))
+                    .subcommand(SubCommand::with_name("help")   // override default help subcommand
+                        .about("display usage"))                // have to manually call after override
+                .get_matches();
+
+    let cmd = if let Some(name) = cli.subcommand_name() {name} else {"help"};
+    if cmd != "start" {
+        println!("{}", cli.usage());    // "start" missing, print help
+        process::exit(0);
+    }
+
+    // read config file
+
+    //let config = cli.value_of("config").unwrap();
+
+    // construct config
+
+    Config {
+        debug: cli.is_present("debug"),
+        port: 1045,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,23 @@
 #[macro_use]
 extern crate clap;
 
-use clap::{App, Arg, SubCommand};
-use std::process;
+mod config;
 
 fn main() {
 
-    let cli = App::new(crate_name!())
-                    .version(crate_version!())
-                    .about("Run IPREF gateway in user space")
-                    .help_message("display this help message")
-                    .version_message("display version")
-                    .arg(Arg::with_name("config")
-                        .short("c")
-                        .long("config")
-                        .value_name("CONFIG")
-                        .default_value("/etc/ipref/ipref.conf")
-                        .help("absolute path to config file")
-                        .takes_value(true))
-                    .args_from_usage("-d, --debug 'enable debug'")
-                    .subcommand(SubCommand::with_name("start")
-                        .about("start IPREF gateway"))
-                    .subcommand(SubCommand::with_name("help")   // override default help subcommand
-                        .about("display usage"))                // have to manually call after override
-                .get_matches();
+    /* Get config
+     *
+     *  Ideally, cfg would be acessible globally from any thread without locks
+     *  and without fear.
+     *
+     *  Currently, rust makes such an arrangement very difficult to implement
+     *  without degrading to 'unsafe'. For now, we're going to pass cfg around
+     *  as a parameter.
+     */
 
-    let cmd = if let Some(name) = cli.subcommand_name() {name} else {"help"};
-    if cmd != "start" {
-        println!("{}", cli.usage());    // "start" missing, print help
-        process::exit(0);
-    }
+    let cfg = config::get();
 
-    let config = cli.value_of("config").unwrap();
-    let debug = if cli.is_present("debug") {true} else {false};
-
-    println!("cli params:");
-    println!("    config: {}", config);
-    println!("    debug:  {}", debug);
+    println!("configuration:");
+    println!("    port:   {}", cfg.port);
+    println!("    debug:  {}", cfg.debug);
 }
