1c9faeb19d7303a06ae72999adff680512b0f39d
waldemar <waldemar@wdmsys.com>
2018-07-14 19:35:54 -0700

    add logger

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@
 
 #[macro_use]
 extern crate clap;
+#[macro_use]
+extern crate log;
+extern crate flexi_logger;
+extern crate chrono;
+
+use flexi_logger::Logger;
+use std::io;
+use chrono::Local;
 
 mod config;
 
@@ -22,4 +30,27 @@ fn main() {
     println!("configuration:");
     println!("    port:   {}", cfg.port);
     println!("    debug:  {}", cfg.debug);
+
+    Logger::with_env_or_str(if cfg.debug {"meadow=debug"}else{"meadow=info"})
+        .format(log_formt)
+        .start()
+        .unwrap();
+
+    trace!("trace message");
+    debug!("debug message");
+    info!("info message");
+    warn!("warn message");
+    error!("error message");
+}
+
+fn log_formt(ww: &mut io::Write, rec: &log::Record) -> Result<(), io::Error> {
+
+    write!(ww, "{} {:5} [{}] {}:{}: {}",
+        Local::now().format("%H:%M:%S%.6f"),
+        rec.level(),
+        rec.module_path().unwrap_or("<unnamed>"),
+        rec.file().unwrap_or("<unnamed>"),
+        rec.line().unwrap_or(0),
+        &rec.args()
+    )
 }
