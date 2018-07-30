8c85a75ae800fe235132df7400295699cd18dbd6
waldemar <waldemar@wdmsys.com>
2018-07-29 21:17:46 -0700

    add dns module

---
+++ src/dns.rs
@@ -0,0 +1,10 @@
+/* Copyright (c) 2018 Waldemar Augustyn */
+
+#![allow(unused_variables)]
+
+use mapper::Mapper;
+use config::Config;
+
+pub fn thread_dns_watcher(cfg: &Config, map: &Mapper) {
+
+}
