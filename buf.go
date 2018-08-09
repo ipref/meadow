de07a039c6bed4cd62fe0d5c5d7ea85c7fab14b0
waldemar <waldemar@wdmsys.com>
2018-08-09 06:11:44 -0700

    qualiy packets

--- buf.go
+++ buf.go
@@ -6,6 +6,43 @@ const (
 	MAXBUF = 10
 )
 
+const ( // packet handling verdicts
+
+	ACCEPT = iota + 1
+	DROP
+	STOLEN
+)
+
+const ( // internal packet constants (Ipv1)
+
+	V1_HDRLEN = 16
+	V1_CMDIX  = 1
+	V1_SRCQIX = 2
+	V1_DSTQIX = 3
+	V1_MARKIX = 4
+)
+
+const ( // data item types
+
+	V1_AREC = iota + 1
+)
+
+const ( // internal packet types
+
+	V1_PKT_AREC = iota + 1
+	V1_PKT_TMR
+)
+
+const ( // AREC commands
+
+	V1_SET_HOSTS_REC = iota + 1
+)
+
+const ( // TMR commands
+
+	V1_PURGE_EXPIRED = iota + 1
+)
+
 type PktBuf struct {
 	pkt     []byte
 	data    uint
@@ -14,6 +51,42 @@ type PktBuf struct {
 	udphdr  uint
 	tcphdr  uint
 	icmphdr uint
+	arechdr uint
+	tmrhdr  uint
+}
+
+func (pb *PktBuf) len() int {
+	return int(pb.tail - pb.data)
+}
+
+func (pb *PktBuf) qualify() {
+
+	pblen := pb.len()
+
+	if pblen == 0 { // empty pkt
+		return
+	}
+
+	if (pb.pkt[pb.data] & 0xf0) == 0x40 { // IPv4 pkt
+		pb.iphdr = pb.data
+		return
+	}
+
+	// we use IPv1 for internal packet types
+
+	if (pb.pkt[pb.data]&0xf0) != 0x10 || pblen < 16 {
+		log.err("PktBuf: unexpected packet type: %v len(%v)", pb.pkt[pb.data]>>4, pblen)
+		return
+	}
+
+	switch pb.pkt[pb.data] & 0x0f {
+	case V1_PKT_AREC:
+		pb.arechdr = pb.data
+	case V1_PKT_TMR:
+		pb.tmrhdr = pb.data
+	default:
+		log.err("PktBuf: unknown packet type: %v", pb.pkt[pb.data]&0x0f)
+	}
 }
 
 var getbuf chan (*PktBuf)
@@ -47,6 +120,10 @@ func pkt_buffers() {
 
 		pb.data = 0
 		pb.tail = 0
+		pb.iphdr = 0
+		pb.arechdr = 0
+		pb.tmrhdr = 0
+
 		getbuf <- pb
 	}
 }
