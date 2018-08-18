911f76befd706e0812dfbb053267e8fdc30ef9d5
waldemar <waldemar@wdmsys.com>
2018-08-18 08:26:16 -0700

    move owner and timer to gen.go

--- gen.go
+++ gen.go
@@ -4,6 +4,9 @@ package main
 
 import (
 	"crypto/rand"
+	"strings"
+	"sync"
+	"time"
 )
 
 /* Address and reference allocation
@@ -20,6 +23,68 @@ const (
 	GENQLEN     = 2
 	SECOND_BYTE = 100
 )
+
+const (
+	MAPPER_TMOUT   = 1800                          // [s] mapper record timeout
+	MAPPER_REFRESH = MAPPER_TMOUT - MAPPER_TMOUT/4 // [s] when to refresh
+	MAPPER_TICK    = 4567                          // [ms] timer tick
+)
+
+type Owners struct {
+	oids []string
+	mtx  sync.Mutex
+}
+
+func (o *Owners) init() {
+	o.oids = make([]string, 1, 16)
+	o.oids[0] = "none"
+}
+
+func (o *Owners) name(oid uint32) string {
+	name := "unknown"
+	o.mtx.Lock()
+	if int(oid) < len(o.oids) {
+		name = o.oids[oid]
+	}
+	o.mtx.Unlock()
+	ix := strings.LastIndex(name, "/")
+	if ix < 0 {
+		return name
+	}
+	return name[ix+1:]
+}
+
+func (o *Owners) new_oid(name string) uint32 {
+
+	if len(name) == 0 {
+		log.fatal("owners: missing owner name")
+	}
+
+	o.mtx.Lock()
+	oid := uint32(len(o.oids))
+	o.oids = append(o.oids, name)
+	o.mtx.Unlock()
+	log.debug("owners: new oid: %v(%v)", name, oid)
+	return oid
+}
+
+type Mark struct {
+	base time.Time
+}
+
+func (m *Mark) init() {
+
+	m.base = time.Now().Add(-time.Second) // make sure marker.now() is always > 0
+}
+
+func (m *Mark) now() uint32 {
+
+	return uint32(time.Now().Sub(m.base) / time.Second)
+
+}
+
+var marker Mark
+var owners Owners
 
 var random_dns_ref chan Ref
 var random_mapper_ref chan Ref
@@ -86,3 +151,12 @@ func gen_mapper_refs() {
 
 //var random_dns_ea chan uint32
 //var random_mapper_ea chan uint32
+
+func timer() {
+
+	ticker := time.Tick(MAPPER_TICK * time.Millisecond)
+
+	for _ = range ticker {
+		// nothing for now
+	}
+}
