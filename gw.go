bb7da28f1b69d4f0b234c5b8350a2b5505226559
waldemar <waldemar@wdmsys.com>
2018-08-19 10:05:23 -0700

    use reflen method in gw

--- gw.go
+++ gw.go
@@ -15,23 +15,20 @@ func echo_discard(pb *PktBuf) int {
 	}
 
 	pb.set_iphdr()
-	pb.set_udphdr()
 	pkt := pb.pkt[pb.iphdr:pb.tail]
 
-	udp := pb.udphdr - pb.iphdr
+	udp := uint((pkt[IP_VER] & 0xf) * 4)
 	encap := udp + 8
 	opt := encap + 4
 
-	if len(pkt) < int(opt+4) ||
-		pkt[IP_PROTO] != UDP ||
-		be.Uint16(pkt[udp+UDP_SPORT:udp+UDP_SPORT+2]) != IPREF_PORT ||
-		pkt[opt+OPT_OPT] != IPREF_OPT {
+	reflen := pb.reflen(pb.iphdr)
 
+	if reflen == 0 {
 		log.err("gw echo: not an ipref packet data/end(%v/%v), dropping", pb.data, len(pb.pkt))
 		return DROP
 	}
 
-	payudp := opt + uint(pkt[opt+OPT_LEN])
+	payudp := int(opt) + reflen
 
 	if len(pkt) < int(payudp+8) {
 		log.err("gw echo: short ipref packet data/end(%v/%v), dropping", pb.data, len(pb.pkt))
@@ -81,7 +78,7 @@ func echo_discard(pb *PktBuf) int {
 	copy(ip, pkt[IP_SRC:IP_SRC+4])
 	copy(optport, pkt[udp+UDP_SPORT:udp+UDP_SPORT+2])
 	ttl = pkt[encap+ENCAP_TTL]
-	if pkt[opt+OPTLEN] == IPREF_OPT64_LEN {
+	if reflen == IPREF_OPT64_LEN {
 		copy(ref_l, pkt[opt+OPT_SREF64:opt+OPT_SREF64+8])
 	} else {
 		copy(ref_h, pkt[opt+OPT_SREF128:opt+OPT_SREF128+8])
@@ -92,7 +89,7 @@ func echo_discard(pb *PktBuf) int {
 	copy(pkt[IP_SRC:IP_SRC+4], pkt[IP_DST:IP_DST+4])
 	copy(pkt[udp+UDP_SPORT:udp+UDP_SPORT+2], pkt[udp+UDP_DPORT:udp+UDP_DPORT+2])
 	pkt[encap+ENCAP_TTL] = pkt[encap+ENCAP_HOPS]
-	if pkt[opt+OPTLEN] == IPREF_OPT64_LEN {
+	if reflen == IPREF_OPT64_LEN {
 		copy(pkt[opt+OPT_SREF64:opt+OPT_SREF64+8], pkt[opt+OPT_DREF64:opt+OPT_DREF64+8])
 	} else {
 		copy(pkt[opt+OPT_SREF128:opt+OPT_SREF128+8], pkt[opt+OPT_DREF128:opt+OPT_DREF128+8])
@@ -103,7 +100,7 @@ func echo_discard(pb *PktBuf) int {
 	copy(pkt[IP_DST:IP_DST+4], ip)
 	copy(pkt[udp+UDP_DPORT:udp+UDP_DPORT+2], optport)
 	pkt[encap+ENCAP_HOPS] = ttl
-	if pkt[opt+OPTLEN] == IPREF_OPT64_LEN {
+	if reflen == IPREF_OPT64_LEN {
 		copy(pkt[opt+OPT_DREF64:opt+OPT_DREF64+8], ref_l)
 	} else {
 		copy(pkt[opt+OPT_DREF128:opt+OPT_DREF128+8], ref_h)
